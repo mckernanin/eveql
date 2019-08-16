614eb42cfb6e98ade73ca04b7bfe799af7c621e2
Kevin McKernan <kevin@mckernan.in>
2019-08-15 18:54:59 -0600

    initial commit with swagger-to-graphql

---
+++ api/index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from "apollo-server-micro";
+import graphQLSchema from "swagger-to-graphql";
+
+const proxyUrl = "https://esi.evetech.net/";
+const pathToSwaggerSchema = `${__dirname}/swagger.json`;
+console.log(pathToSwaggerSchema);
+
+let server = undefined;
+
+const createServer = async () => {
+  if (server) return server;
+  server = new ApolloServer({
+    schema: graphQLSchema(pathToSwaggerSchema, proxyUrl),
+    playground: true
+  });
+  return server;
+};
+
+export default async function(req, res) {
+  const server = await createServer();
+  const handler = server.createHandler({ path: "/api/" });
+  return handler(req, res);
+}
