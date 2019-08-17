4c0f39137048d687f29712a3b7d5029cbd8d7533
Kevin McKernan <kevin@mckernan.in>
2019-08-17 16:01:03 -0600

    add now config, move static files to subdir, add apollo engine

--- api/index.ts
+++ api/index.ts
@@ -4,25 +4,29 @@ import graphQLSchema from "swagger-to-graphql";
 
 const proxyUrl = "https://esi.evetech.net/latest";
 const pathToSwaggerSchema = `${__dirname}/swagger.json`;
-console.log(pathToSwaggerSchema);
+
+interface Headers {
+  authorization?: string;
+  "User-Agent": string;
+}
 
 let server = undefined;
 
-const createServer = async headers => {
+const createServer = async (headers: Headers) => {
   if (server) return server;
   server = new ApolloServer({
     schema: graphQLSchema(pathToSwaggerSchema, proxyUrl, headers),
     playground: true,
-    introspection: true
+    introspection: true,
+    engine: {
+      apiKey: process.env.ENGINE_API_KEY
+    }
   });
   return server;
 };
 
 export default async function(req: NowRequest, res: NowResponse) {
-  const headers: {
-    authorization?: string;
-    "User-Agent": string;
-  } = {
+  const headers: Headers = {
     "User-Agent": "eveql - https://eveql.xyz"
   };
   if (req.headers.authorization) {
